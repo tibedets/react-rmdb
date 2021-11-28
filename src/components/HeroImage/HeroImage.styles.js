7d260da36b0309d35cebb3a3d699be826de52862
Thaddeus Brown <tibedets@gmail.com>
2021-11-28 16:49:50 -0600

    HeroImage Component

--- src/components/HeroImage/HeroImage.styles.js
+++ src/components/HeroImage/HeroImage.styles.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   background: linear-gradient(
-    to bottom, 
-    rgba(0,0,0,0)
-    41%,rgba(0,0,0,0.65) 100%
-  ),
-  url(${({ image }) => image}), var(--darkGrey);
+      to bottom,
+      rgba(0, 0, 0, 0) 41%,
+      rgba(0, 0, 0, 0.65) 100%
+    ),
+    url(${({ image }) => image}), var(--darkGrey);
   background-size: 100%, cover;
   background-position: center;
   height: 600px;
