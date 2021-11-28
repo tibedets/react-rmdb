0a70a0788f2f722f9c2266a3c954de0bd496b514
Thaddeus Brown <tibedets@gmail.com>
2021-11-28 16:48:54 -0600

    HeroImage Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //Components
-
+import HeroImage from "./HeroImage";
 //Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 //Image
@@ -13,7 +13,17 @@ const Home = () => {
 
   console.log(state);
 
-  return <div>Home Page</div>;
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
