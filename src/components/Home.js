2f8517afa355e59060a816e4ca9446ed85a608ee
Thaddeus Brown <tibedets@gmail.com>
2021-11-29 14:06:27 -0600

    Grid and Thumb Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //Components
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+import Thumb from "./Thumb";
 //Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 //Image
@@ -22,6 +24,20 @@ const Home = () => {
           text={state.results[0].overview}
         />
       ) : null}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path 
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
+        ))}
+      </Grid>
     </>
   );
 };
