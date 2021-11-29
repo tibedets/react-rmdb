c212cf2275b9bba6f47516cfcc558632872896ac
Thaddeus Brown <tibedets@gmail.com>
2021-11-29 15:14:16 -0600

    Search Bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,26 +6,28 @@ import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
 //Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 //Image
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
 
   console.log(state);
 
   return (
     <>
-      {state.results[0] ? (
+      {state.results[0] ? ( //"{!searchTerm && state.results[0] ? ( " -- no HeroImage
         <HeroImage
           image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
           title={state.results[0].original_title}
           text={state.results[0].overview}
         />
       ) : null}
-      <Grid header="Popular Movies">
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <Grid header={searchTerm ? 'Search Result' : "Popular Movies"}>
         {state.results.map((movie) => (
           <Thumb
             key={movie.id}
