9b63f6a32474f1ab6f0074df22e0c9370a5bddde
Thaddeus Brown <tibedets@gmail.com>
2021-11-29 20:47:24 -0600

    MovieInfo

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,6 +6,7 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb";
+import MovieInfo from './MovieInfo';
 //Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
 //Image
@@ -22,6 +23,7 @@ const Movie = () => {
   return (
     <>
       <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
     </>
   );
 };
